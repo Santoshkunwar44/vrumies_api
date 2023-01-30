a9ec9f4f7b86e140af2d710e535c835bfe595431
santosh 2 <santehero8@gmail.com>
2023-01-30 15:31:38 +0545

    cookie

--- index.js
+++ index.js
@@ -20,8 +20,10 @@ app.use(cors({
 }))
 
 app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', req.headers.origin);
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    console.log(req.headers.origin)
+    res.header("Access-Control-Allow-Headers", "*");
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     next();
 });
 
@@ -49,7 +51,7 @@ app.use(session({
     cookie: {
         secure: true,
         maxAge: 31556952000,
-        httpOnly: false,
+        httpOnly: true,
         sameSite: "none",
     },
 }))
