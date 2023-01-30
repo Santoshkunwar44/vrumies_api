7a2f05fc3a429f0d3a125a997b4c6358d4cdd603
santosh 2 <santehero8@gmail.com>
2023-01-30 23:02:55 +0545

    major cahgnes

--- services/authService/AuthService.js
+++ services/authService/AuthService.js
@@ -68,7 +68,7 @@ class AuthService {
 
         let remainingTimeInMs = currentTimeInMs - lastLoggedIn;
 
-        
+
 
 
 
--- services/authService/TokenService.js
+++ services/authService/TokenService.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken")
-
+const RefreshToken = require("../../model/refreshToken")
 class TokenService {
 
 
