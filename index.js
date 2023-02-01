35356ef1832c2ce5cd29f76fc2033c7e4c36ef64
santosh 2 <santehero8@gmail.com>
2023-02-01 14:32:03 +0545

    hello 8

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(cors({
 }))
 app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Credentials', true);
-    res.header('Access-Control-Allow-Origin', req.headers.origin);
+    res.header('Access-Control-Allow-Origin', "https://candid-capybara-9e49cc.netlify.app");
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,UPDATE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
     next();
